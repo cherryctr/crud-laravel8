6607779931aa7b63729d064412ccba7624e41f9b
cherryctr <chrryctr1509@gmail.com>
2021-02-19 14:53:07 +0700

    Balikin ke semula

--- resources/views/livewire/create.blade.php
+++ resources/views/livewire/create.blade.php
@@ -15,7 +15,7 @@
       <div class="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
        
            <div>
-             <h1 class="font-bold text-center mb-4">CREATING</h1>
+             <h1 class="font-bold text-center mb-4">CREATE</h1>
            </div>
 
            <div>
